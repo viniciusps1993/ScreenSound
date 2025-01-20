75a2ed72d31ddd5158eff5b79a861af4245a11a1
viniciusps1993 <vpsndv@gmail.com>
2025-01-20 14:05:19 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,26 @@
+﻿class Musica 
+{   
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Dísponivel no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o Plano Plus+");
+        }
+    }
+}
+
+
+
+
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,15 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 359;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 456;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
