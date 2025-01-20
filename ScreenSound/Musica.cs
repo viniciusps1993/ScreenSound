2164dc91a378b2eed21fbd035336e50f6b5cea77
viniciusps1993 <vpsndv@gmail.com>
2025-01-20 17:40:14 -0300

    Alterando get com lambda

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -1,16 +1,17 @@
 ﻿class Musica 
 {   
-    public string nome;
-    public string artista;
-    public int duracao;
+    public string Nome {  get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
     public bool Disponivel { get; set; }
-    
-
+    public String DescricaoResumida => 
+        $"A música {Nome} pertence á banda {Artista}";
+ 
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração: {duracao}");
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
         if (Disponivel)
         {
             Console.WriteLine("Dísponivel no plano.");
