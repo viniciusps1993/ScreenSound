c41c80b17d4328cd94110c95ef1139570721e9e8
viniciusps1993 <vpsndv@gmail.com>
2025-01-26 19:11:36 -0300

    Concluindo a classe Podcast

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,7 +1,13 @@
-﻿Episodio ep1 = new(4, "Técnicas de Facilitação", 45);
-Console.WriteLine(ep1.Resumo);
-
+﻿Episodio ep1 = new(1, "Técnicas de Facilitação", 45);
 ep1.AdicionarConvidados("Maria");
 ep1.AdicionarConvidados("Marcelo");
 
-Console.WriteLine(ep1.Resumo);
+Episodio ep2 = new(2, "Técnicas de Aprendizado", 55);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Flavia");
+ep2.AdicionarConvidados("Marcio");
+
+Podcast podcast = new Podcast("PodInfo", "Vinicius");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
